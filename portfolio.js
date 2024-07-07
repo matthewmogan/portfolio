b82663ccc31a912c3a7d1e2252e57a45e845396e
matthewmogan <mattmogan@gmail.com>
2024-07-07 20:52:38 +0300

    fixed padding issue of masthead at tablet size and updated link formatting

--- portfolio.js
+++ portfolio.js
@@ -31,39 +31,3 @@ function checkScreenWidth() {
 
 checkScreenWidth()
 window.addEventListener('resize',checkScreenWidth)
-
-// ----- SMOOTH NAVIGATION TO PORTFOLIO SECTIONS WHEN IN FULL SCREEN MODE ----- //
-
-// const root = document.documentElement;
-// // Get the computed font size of the root element
-// const rem = window.getComputedStyle(root).getPropertyValue('font-size');
-// console.log(rem)   
-
-// function offsetScroll(event, sectionId) {
-//     event.preventDefault();
-//     const yOffset = -parseInt(rem)*6; // Adjust this value to the desired offset on nav bar click
-//     const element = document.getElementById(sectionId);
-//     const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-//     window.scrollTo({top: y, behavior: 'smooth'});
-// }
-
-
-
-// add hover effect method to all hoverElements
-// let hoverElements = document.getElementsByClassName('hoverEffect')
-
-// console.log(hoverElements)
-// console.log(hoverElements.length)
-
-// function hoverIn(event){
-//     event.target.style.backgroundColor = "rgba(240,240,240,.25)";
-// }
-
-// function hoverOut(event){
-//     event.target.style.backgroundColor = ""
-// }
-
-// for (let i=0; i<hoverElements.length; i++){
-//     hoverElements[i].addEventListener("mouseover",hoverIn)
-//     hoverElements[i].addEventListener("mouseout",hoverOut) 
-// }
